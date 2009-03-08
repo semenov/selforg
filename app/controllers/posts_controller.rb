678aacca22d218e7edc2f79f51fe897c29f15fcc
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-03-09 01:57:39 +0600

    Dirty nil prototype

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  # GET /posts
+  # GET /posts.xml
+  def index
+    @posts = Post.published
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @posts }
+    end
+  end
+
+  # GET /posts/1
+  # GET /posts/1.xml
+  def show
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @post }
+    end
+  end
+
+  # GET /posts/new
+  # GET /posts/new.xml
+  def new
+    @post = Post.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @post }
+    end
+  end
+
+  # POST /posts
+  # POST /posts.xml
+  def create
+    @post = Post.new(params[:post])
+
+    respond_to do |format|
+      if @post.save
+        flash[:notice] = 'Post was successfully created.'
+        format.html { redirect_to(@post) }
+        format.xml  { render :xml => @post, :status => :created, :location => @post }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+end
