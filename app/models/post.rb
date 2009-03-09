9ab0f04068ae348c901051038137c895faa74c7c
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-03-09 15:49:04 +0600

    Fixed posts order

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title, :content
-  named_scope :published, :conditions => {:published => true}  
+  named_scope :published, :conditions => {:published => true} , :order => "created_at DESC"  
 end
