784975bcc9ebd3c147f427b0d3e9a5e908c40dd4
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-03-29 17:42:42 +0700

    Now an excerpt is shown in a post listing instead of a full content

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title, :content
   named_scope :published, :conditions => {:published => true} , :order => "created_at DESC"  
+  
+  PageSeparator = /^\s*---\s*$/
+  
+public
+  
+  def excerpt
+    content.split(PageSeparator).first
+  end
+
+  def has_more?
+    content.index(PageSeparator)
+  end
+  
 end
